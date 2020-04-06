e35fb7d5246e47f0eb2ddff01f3ff65dd36d1069
soundreaper <soundreaper@gmail.com>
2020-04-05 22:53:02 -0700

    add two files for each problem

---
+++ hw_2_problem_1.py
@@ -0,0 +1,18 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+"""
+
+def twoSum(nums, target):
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))
